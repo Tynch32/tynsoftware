8a82be3aa45b88fda2b877d1d5f4f56c8954f2b5
Martin <velazquezmartingonzalo@gmail.com>
2024-11-23 16:42:00 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import remix from '@remix-run/vite';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist/client' // Cambia el directorio de salida
-  }
+  plugins: [remix()]
 });
