714630292c6bfb0d40a23b5623d1019597ac9beb
Martin <velazquezmartingonzalo@gmail.com>
2024-11-23 16:46:09 -0300

    achis

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import remix from '@remix-run/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [remix()]
+  plugins: [remix(), react()],
 });
